3032f65829bf124e7eea1dbebc0c57b3e98d660a
Tomas Trnecka <trnecka@gmail.com>
2019-03-29 20:46:23 +0000

    crude webpackge with basic collections

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
+import sys
+sys.path.insert(0, '/home/trnecka/imperium-bot')
 
-from flask import Flask
+from coach import Coach
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Imperium Web!'
+@app.route("/")
+def index():
+    return render_template("index.html", coaches = Coach.all())
 
