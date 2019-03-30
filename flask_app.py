4c4bcaa7d693e327943568efdc508fe10c8e3eec
Tomas Trnecka <trnecka@gmail.com>
2019-03-30 16:12:45 +0100

    added rarity icons

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 
 # A very simple Flask Hello World app for you to get started with...
 import sys
-sys.path.insert(0, '/home/trnecka/imperium-bot')
+#sys.path.insert(0, '/home/trnecka/imperium-bot')
+sys.path.insert(0, '../imperium-bot')
 
 from coach import Coach
 
@@ -14,3 +15,6 @@ app.config["DEBUG"] = True
 def index():
     return render_template("index.html", coaches = Coach.all())
 
+# run the application
+if __name__ == "__main__":  
+    app.run(debug=True)
