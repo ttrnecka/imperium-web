0e0d56be3984faf77632ea0a5013478e87a8dfed
Tomas Trnecka <trnecka@gmail.com>
2019-03-31 23:06:47 +0200

    niced up the view a bit plus ordering

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 import sys
-sys.path.insert(0, '/home/trnecka/imperium-bot')
-#sys.path.insert(0, '../imperium-bot')
+#sys.path.insert(0, '/home/trnecka/imperium-bot')
+sys.path.insert(0, '../imperium-bot')
 
-from coach import Coach
+#from coach import Coach
+from imperiumbase import Coach, Pack
 
 from flask import Flask, render_template
 
@@ -13,7 +14,8 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def index():
-    return render_template("index.html", coaches = Coach.all())
+    sorted_coached = sorted(Coach.all(), key=lambda x: x.name)
+    return render_template("index.html", coaches = sorted_coached, Pack = Pack)
 
 # run the application
 if __name__ == "__main__":  
